d3cb1d156757f258f6715aae625e65ad85ef3f55
chiamtatyin <chiamtatyin@gmail.com>
2025-07-30 13:23:43 +0800

    Configure Vite for GitHub Pages and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/greencap-website/', // Add this line for GitHub Pages deployment
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, './src'),
     },
   },
 })
